f3983f217f493f38236ec95ac3b55a53624c137f
ishar19 <isharjain19@gmail.com>
2024-07-27 21:55:10 +0530

    added login and register

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/home'
 import Register from './pages/register'
+import Login from './pages/login'
+import { Toaster } from 'react-hot-toast'
+
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <Toaster />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   )
 }
 export default App
--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import { register } from "../services/auth";
-import { useNavigate } from "react-router-dom";
-function Register() {
+import { login } from "../services/auth";
+import toast from "react-hot-toast";
+export default function Login() {
     const [userData, setUserData] = useState({
-        name: null,
         email: null,
         password: null
     })
-    const navigate = useNavigate();
     const handleChange = (e) => {
         setUserData({
             ...userData,
@@ -16,28 +14,27 @@ function Register() {
     }
     const handleSubmit = async (e) => {
         e.preventDefault();
-        if (!userData.name || !userData.email || !userData.password) {
+        if (!userData.email || !userData.password) {
             return;
         }
         try {
-            const { name, email, password } = userData;
-            const response = await register({ name, email, password })
+            const { email, password } = userData;
+            const response = await login({ email, password })
             console.log(response);
             if (response.status === 200) {
-                alert('User created successfully');
-                navigate('/login');
-
+                const { data } = response;
+                localStorage.setItem('token', data.token);
+                toast.success('User logged in successfully');
             }
         }
         catch (error) {
-            console.log(error.message);
+            toast.error("login failed");
         }
     }
     return (
         <div>
-            <h1>Create an account</h1>
+            <h1>Login</h1>
             <form onSubmit={handleSubmit}>
-                <input name="name" value={userData.name} onChange={handleChange} type="text" placeholder="Your name" />
                 <input name="email" value={userData.email} onChange={handleChange} type="email" placeholder="Your email" />
                 <input name="password" value={userData.password} onChange={handleChange} type="password" placeholder="Your password" />
                 <button type="submit">Submit</button>
@@ -45,6 +42,3 @@ function Register() {
         </div>
     );
 }
-
-
-export default Register;
